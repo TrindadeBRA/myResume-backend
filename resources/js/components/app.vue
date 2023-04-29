a5bcb3df1eafd3d46f19a858e6b51571febbf569
Lucas Trindade <trindadebra@gmail.com>
2023-04-29 16:59:49 -0300

    add classes tailwind

--- resources/js/components/app.vue
+++ resources/js/components/app.vue
@@ -1,21 +1,22 @@
 <template>
-  <div>
-    <p>O contador está em: {{ contador }}</p>
-    <button @click="incrementar">Incrementar</button>
-  </div>
+    <div class="flex flex-col items-center justify-center h-screen">
+      <p class="text-2xl mb-4">O contador está em: {{ contador }}</p>
+      <button @click="incrementar" class="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600">Incrementar</button>
+    </div>
 </template>
-
+  
 <script>
-export default {
-  data() {
-    return {
-      contador: 0,
-    };
-  },
-  methods: {
-    incrementar() {
-      this.contador++;
+  export default {
+    data() {
+      return {
+        contador: 0,
+      };
     },
-  },
-};
+    methods: {
+      incrementar() {
+        this.contador++;
+      },
+    },
+  };
 </script>
+  
