d6cac45b5a257cbe549763b57a329426a240d390
Lucas Trindade <trindadebra@gmail.com>
2023-04-29 16:57:06 -0300

    integrando vue.js

---
+++ resources/js/components/app.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p>O contador está em: {{ contador }}</p>
+    <button @click="incrementar">Incrementar</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      contador: 0,
+    };
+  },
+  methods: {
+    incrementar() {
+      this.contador++;
+    },
+  },
+};
+</script>
